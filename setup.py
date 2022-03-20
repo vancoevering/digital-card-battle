fa7f68af67b00eeaeb7f14cdd345eb69cbb106ed
Sartolas <taylorvancoevering@gmail.com>
2022-03-19 21:31:33 -0500

    Updated setup.py to fix issues with src/

--- setup.py
+++ setup.py
@@ -1,3 +1,21 @@
-from setuptools import setup
+from pathlib import Path
+from setuptools import setup, find_packages
 
-setup()
+HERE = Path(__file__).parent
+README = HERE / "README.md"
+
+setup(
+    name="ddcb",
+    description="A terminal game inspired by the psone classic.",
+    long_description=README.read_text(),
+
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[
+        "requests",
+        "beautifulsoup4",
+    ],
+
+    author="Taylor VanCoevering",
+)
